adf6275f02c5ae206ac3b67f2a3db9b4dead1095
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2020-05-09 15:43:16 +0200

    fixed `const` for factorial array Gives x10 in speed

--- src/factorials.jl
+++ src/factorials.jl
@@ -1,5 +1,5 @@
 
-logfact = [0,[sum(log(i) for i=1:n) for n=1:50]...];
+const logfact = [0,[sum(log(i) for i=1:n) for n=1:50]...];
 
 """
     f_logfact(n)
@@ -15,7 +15,7 @@ end
 # special function used in the code:
 # (two_n/2)! for even numbers
 # (two_n-1/2)! for odd numbers
-logfact2 = [f_logfact(div(two_n,2)) for two_n=1:100];
+const logfact2 = [f_logfact(div(two_n,2)) for two_n=1:100];
 function f_logfact2(two_n)
     (two_n < 0 || two_n > 100) && error("two_n < 0 || two_n > 100. Modify if needed.")
     @inbounds return logfact2[two_n+1]
